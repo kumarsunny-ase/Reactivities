8f3ddb00f3a186da2dfbbe5268b27a98069fd185
Kumar Sunny <kumarsunny1348@gmail.com>
2023-03-30 11:06:16 +0200

    End of section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,14 +1,15 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import "semantic-ui-css/semantic.min.css";
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
+  <StoreContext.Provider value={store}>
     <App />
+  </StoreContext.Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
