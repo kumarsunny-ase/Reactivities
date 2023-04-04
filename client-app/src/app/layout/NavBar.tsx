2e0a9122e8fe646da4f9090a56c2b42471a725ab
Kumar Sunny <kumarsunny1348@gmail.com>
2023-04-04 22:36:16 +0200

    End of section 8

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,19 +1,18 @@
 import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
-import { useStore } from "../stores/store";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-const {activityStore} = useStore();
 
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
+          <Menu.Item header as={NavLink} to='/'>
             <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
             Reactivities
           </Menu.Item>
-          <MenuItem name='Activities' />
+          <MenuItem name='Activities' as={NavLink} to='/activities' />
           <MenuItem>
-          <Button onClick={() => activityStore.openForm()} positive content='Create Activity' />
+          <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
           </MenuItem>
         </Container>
       </Menu>
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,21 @@
-import { Button, ButtonGroup, Card, Image } from "semantic-ui-react";
+import {  Button, ButtonGroup, Card, Image } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 
-export default function ActivityDetails(){
+export default observer (function ActivityDetails(){
 const {activityStore} = useStore();
-const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+const {id} = useParams();
 
-if (!activity) return <LoadingComponent />;
+useEffect(() => {
+  if (id) loadActivity(id);
+}, [id, loadActivity])
+
+if (loadingInitial || !activity) return <LoadingComponent />;
 
     return(
 
@@ -24,10 +32,10 @@ if (!activity) return <LoadingComponent />;
     </Card.Content>
     <Card.Content extra>
       <ButtonGroup widths='2'>
-        <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit'/>
-        <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel'/>
+        <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit'/>
+        <Button as={Link} to='/activities'  basic color='grey' content='Cancel'/>
       </ButtonGroup>
     </Card.Content>
   </Card>
 )
-}
+})
