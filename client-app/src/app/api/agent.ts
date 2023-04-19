fb5f400ada286624b0abac2e034f17c4f9ffba9d
Kumar Sunny <kumarsunny1348@gmail.com>
2023-04-19 08:11:42 +0200

    End of section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from "../Models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
+import { User, UserFormValues } from "../Models/user";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -12,7 +13,13 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
-// const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if(token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
 
 axios.interceptors.response.use(async response => {
         await sleep(1000);
@@ -54,7 +61,6 @@ axios.interceptors.response.use(async response => {
     return Promise.reject(error);
 })
 
-const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
 const request = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
@@ -71,8 +77,15 @@ const Activities = {
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
+const Account = {
+    current: () => request.get<User>('account'),
+    login: (user: UserFormValues) => request.post<User>('/account/login', user),
+    register: (user: UserFormValues) => request.post<User>('/account/register', user)
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,21 +1,50 @@
-import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import { Button, Container, Menu, MenuItem, Image, Dropdown } from "semantic-ui-react";
+import { Link, NavLink } from "react-router-dom";
+import { useStore } from "../stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function NavBar() {
-
+export default observer(function NavBar() {
+const { userStore: {user, logout} } = useStore();
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header as={NavLink} to='/'>
-            <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
+          <Menu.Item header as={NavLink} to="/">
+            <img
+              src="/assets/logo.png"
+              alt="logo"
+              style={{ marginRight: "10px" }}
+            />
             Reactivities
           </Menu.Item>
-          <MenuItem name='Activities' as={NavLink} to='/activities' />
-          <MenuItem name='Errors' as={NavLink} to='/errors' />
+          <MenuItem name="Activities" as={NavLink} to="/activities" />
+          <MenuItem name="Errors" as={NavLink} to="/errors" />
           <MenuItem>
-          <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
+            <Button
+              as={NavLink}
+              to="/createActivity"
+              positive
+              content="Create Activity"
+            />
+          </MenuItem>
+          <MenuItem>
+            <Image
+              src={user?.image || "assets/user.png"}
+              avatar
+              spaced="right"
+            />
+            <Dropdown pointing="top left" text={user?.displayName}>
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  as={Link}
+                  to={`/profile/${user?.username}`}
+                  text="My Profile"
+                  icon="user"
+                />
+                <Dropdown.Item onClick={logout} text="Logout" icon="power" />
+              </Dropdown.Menu>
+            </Dropdown>
           </MenuItem>
         </Container>
       </Menu>
     );
-}
+})
